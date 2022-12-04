065f4320581a2701407e91a2434eee0915283d3a
Moses-Onche <onchemoses38@gmail.com>
2022-12-04 18:16:48 +0100

    Return tuple of length of a string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    invalid = my_list is None or len(my_list) == 0
-    if invalid:
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        max = [0, None]
+        return max
+    else:
+        max = [len(sentence), sentence[0]]
+        return max
+"""    invalid = my_list is None or len(my_list) == 0
+       if invalid:
         return "None"
     max = my_list[0]
     for i in my_list:
         if i > max:
             max = i
-    return max
+    return max """
