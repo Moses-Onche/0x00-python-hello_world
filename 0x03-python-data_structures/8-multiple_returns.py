0f2eec4df5ad8a0c48c26a0edab2e65132852b01
Moses-Onche <onchemoses38@gmail.com>
2022-12-05 19:08:02 +0100

    Return tuple of the length of a string and first chr

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -6,11 +6,3 @@ def multiple_returns(sentence):
     else:
         max = [len(sentence), sentence[0]]
         return max
-"""    invalid = my_list is None or len(my_list) == 0
-       if invalid:
-        return "None"
-    max = my_list[0]
-    for i in my_list:
-        if i > max:
-            max = i
-    return max """
