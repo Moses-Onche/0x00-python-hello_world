35f402882ca9c69505dad6b3d087bdcf5b208078
Moses-Onche <onchemoses38@gmail.com>
2022-12-07 19:31:48 +0100

    Add all unique integers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    '''
-        A function that traverse through a list for an element that matches
-        search and modify it with replace then returns a new list.
-        @elem: Elements
-    '''
-    if len(my_list) == 0:
-        return my_list
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    num = 0
 
-    new_lst = [elem if elem != search else replace for elem in my_list]
-    return new_lst
+    for i in uniq_list:
+        num += i
+
+    return (num)
