9eaf04c2b48d509dc38d5f9150927ba69ceb5fbe
Moses-Onche <onchemoses38@gmail.com>
2023-01-02 17:45:17 +0100

    Define a rectangle class with spec parameters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,9 +21,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("Must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("Must be greater than zero")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
