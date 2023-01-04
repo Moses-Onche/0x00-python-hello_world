4396fb87b806d5612b795b5ca6ab034cf7b34ff8
Moses-Onche <onchemoses38@gmail.com>
2023-01-04 14:11:57 +0100

    Write a function that adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,5 +5,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-
     return (int(a) + int(b))
