3439bbb1de374865c2cadf799d54a18edf8403cc
Moses-Onche <onchemoses38@gmail.com>
2023-01-10 11:22:52 +0100

    Implement str() magic method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a subclass Square of the class Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+            size (int): size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
