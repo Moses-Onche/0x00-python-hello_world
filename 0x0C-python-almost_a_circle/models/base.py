9e89ed1ebc1c3a6cf4315a37c7ed321d314b94ca
Moses-Onche <onchemoses38@gmail.com>
2023-01-16 18:37:16 +0100

    Create the first class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Base"""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Base for all other classes in this project.
+    private class attribute:
+        __nb_objects (int): number of instantiated classes
+    """
+
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new base
+        Args:
+            id (int): new base identifier
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
