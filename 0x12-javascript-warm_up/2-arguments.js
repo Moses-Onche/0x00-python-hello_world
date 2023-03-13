34680958410bbfe6fa9a2427070d63bb33d37b0d
Moses-Onche <onchemoses38@gmail.com>
2023-03-13 14:42:35 +0100

    one or more arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
-var args = process.argv;
-if (args.length === 2) {
-	console.log('No argument');
-} else if (args.length === 3) {
-	console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
