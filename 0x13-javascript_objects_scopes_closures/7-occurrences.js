f5ead4b7e54ca2640781b47e69b59f3b313d8945
Moses-Onche <onchemoses38@gmail.com>
2023-03-14 21:29:06 +0100

    returns the number of occurences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-rts.nbOccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let nOccurrences = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
