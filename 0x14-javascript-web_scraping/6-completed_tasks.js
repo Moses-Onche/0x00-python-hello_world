7a90c5f50c026924a27e7c4b45c81f0bce77be83
Moses-Onche <onchemoses38@gmail.com>
2023-04-25 22:54:30 +0100

    A script computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/* This script computes the number of tasks completed by user id. */
+const request = require('request');
+const requestUrl = process.argv[2];
+
+request(requestUrl, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const count in tasks) {
+      const task = tasks[count];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
