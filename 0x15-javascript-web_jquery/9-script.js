81b54a6acdc23770153145e5f91d1009905c9a4d
Moses-Onche <onchemoses38@gmail.com>
2023-05-06 12:28:28 +0100

    A script that fetches from a URL and displays hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+const greeting = document.getElementById('hello');
+fetch('https://fourtonfish.com/hellosalut/?lang=fr')
+  .then((response) => response.json())
+  .then((data) => {
+	      console.log(data);
+	    })
+  .catch((err) => console.log(err));
